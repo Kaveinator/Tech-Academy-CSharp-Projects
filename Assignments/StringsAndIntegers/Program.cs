2c50a8f2ab3f7a341f9e3621b8136ff2c2dd7603
Kaveman <55042475+Kaveinator@users.noreply.github.com>
2024-03-30 23:05:56 -0700

    Update Program.cs

--- Assignments/StringsAndIntegers/Program.cs
+++ Assignments/StringsAndIntegers/Program.cs
@@ -7,7 +7,7 @@ using static System.Double;
 
 namespace StringsAndIntegers {
     internal class Program {
-        readonly static int[] Integers = new[] { 0, 1, -2, 3, 4, 5, 6, 7, 8, 9, 0}; // Req 264.1
+        readonly static List<int> Integers = new List<int>() { 0, 1, -2, 3, 4, 5, 6, 7, 8, 9, 0}; // Req 264.1
         static void Main(string[] args) {
             // Loop over each integer
             foreach (int num in Integers) { // num => numerator
