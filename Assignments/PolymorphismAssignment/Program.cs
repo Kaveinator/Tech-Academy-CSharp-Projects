1f26eb561b43a8b021b29052480fb30c2dc27c05
Kaveman <55042475+Kaveinator@users.noreply.github.com>
2024-03-31 23:10:11 -0700

    Polymorphism Assignment

---
+++ Assignments/PolymorphismAssignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignment {
+    internal class Program {
+        static void Main(string[] args) {
+            Person person = new Employee(0, "Sample", "Student");
+            if (person is IQuittable quitabledPerson) {
+                quitabledPerson.Quit();
+            }
+            Console.ReadKey(true);
+        }
+    }
+    abstract class Person {
+        public readonly string FirstName;
+        public readonly string LastName;
+
+        public Person(string firstName, string lastName) {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public virtual void SayName() => Console.WriteLine($"{FirstName} {LastName}");
+    }
+    class Employee : Person, IQuittable {
+        public readonly int Id;
+
+        public Employee(int id, string firstName, string lastName)
+            : base(firstName, lastName)
+            => Id = id;
+
+        public override void SayName() {
+            Console.Write($"#{Id}: ");
+            base.SayName();
+        }
+
+        public void Quit() => Console.WriteLine($"Employee #{Id} Quit");
+    }
+    interface IQuittable {
+        void Quit();
+    }
+}
