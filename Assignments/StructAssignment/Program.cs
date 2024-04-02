7b620e72b7b928db5f0342674383f99902ece5d2
Kaveman <55042475+Kaveinator@users.noreply.github.com>
2024-04-02 10:17:58 -0700

    Struct Assignment

---
+++ Assignments/StructAssignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StructAssignment {
+    internal class Program {
+        static void Main(string[] args) {
+            Number<decimal> number = new Number<decimal>(21m); // Create a number struct with a decimal value of 21
+            Console.WriteLine(number); // Print
+            Console.ReadKey(true); // Wait for key press then exit
+        }
+    }
+    public struct Number<T> {
+        public T Ammount;
+        public Number(T ammt) => Ammount = ammt;
+        public override string ToString() => Ammount.ToString();
+    }
+}
