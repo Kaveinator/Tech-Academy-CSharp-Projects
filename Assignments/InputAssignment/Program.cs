e0145823f26d8d3a5f837c955a4f4e99ca9e346c
Kaveman <55042475+Kaveinator@users.noreply.github.com>
2024-04-09 16:39:29 -0700

    Input Assignment

---
+++ Assignments/InputAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using static InputAssignment.ConsoleUtils;
+
+namespace InputAssignment {
+    internal class Program {
+        static void Main(string[] args) {
+            LogFile logger = new LogFile(); // Initilize a log file
+            logger.Log(ReadNumeral<sbyte>(minValue: -100, maxValue: 100)); // Get num from -100 to 100
+            ClearLastLine(); // Erase the question for input
+            logger.Dispose(); // Dispose the log file since no longer needed
+            Console.WriteLine(File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "latest.log"))); // Display the log file
+
+            while (true) _ = Console.ReadKey(true); // Keeps console infinitly open
+        }
+    }
+}
