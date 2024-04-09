d1402d332612bf719f3aa97ac7a2a30499f53b0a
Kaveman <55042475+Kaveinator@users.noreply.github.com>
2024-04-09 16:51:24 -0700

    DateTime Assignment

---
+++ Assignments/DateTimeAssignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static InputAssignment.ConsoleUtils;
+
+namespace DateTimeAssignment {
+    internal class Program {
+        static void Main(string[] args) {
+            DateTime startTime = DateTime.Now;
+            Console.WriteLine(startTime); // Print current time
+            sbyte hoursToAdd = ReadNumeral<sbyte>(minValue: -100, maxValue: 100);
+            ClearLastLine(); // Clears the input
+            Console.WriteLine(startTime.AddHours(hoursToAdd)); // Print datetime with added horus
+
+            while (true) _ = Console.ReadKey(true);
+        }
+    }
+}
