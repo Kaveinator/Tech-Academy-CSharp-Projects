b0a794ad133f55926f7243160155b027dfc4091b
Kaveman <55042475+Kaveinator@users.noreply.github.com>
2024-04-09 19:00:22 -0700

    Try/Catch Assignment

---
+++ Assignments/TryCatchAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static InputAssignment.ConsoleUtils;
+
+namespace TryCatchAssignment {
+    internal class Program {
+        static void Main(string[] args) {
+            try {
+                byte age = ReadNumeral<byte>("Age:", 0, 100); // Read age between 0 and 100 (inclusive)
+                if (age == 0) // If age is 0, throw an error
+                    throw new AgeException("You can't be using a computer as a newborn! Where are your parents XD");
+                if (age < 18)
+                    throw new AgeException("You need a parent or guardian to fill out this form for you");
+                Console.WriteLine($"You were born in {DateTime.Now.Year - age}"); // Show the year you were born
+            } catch (AgeException e) {
+                Console.WriteLine($"Age Restriction: {e.Message}");
+            } catch (Exception e) {
+                Console.WriteLine($"Internal Error: {e}");
+            }
+
+            while (true) _ = Console.ReadKey(true);
+        }
+    }
+    public class AgeException : Exception {
+        public AgeException(string msg) : base(msg) { }
+    }
+}
