6f5a016abc11ed0328e14eb3b44f48d40078c5f3
Kaveman <55042475+Kaveinator@users.noreply.github.com>
2024-04-09 17:10:33 -0700

    Constructor Assignment

---
+++ Assignments/ConstructorAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConstructorAssignment {
+    internal class Program {
+        static void Main(string[] args) {
+            var user = new User("User1");
+            user = new User(new Guid(), "Zero'd out"); // Create users
+        }
+    }
+    public class User {
+        public readonly Guid Id;
+        public string DisplayName { get; protected set; }
+
+        public User(string displayName) : this(Guid.NewGuid(), displayName) { }
+
+        public User(Guid id, string displayName) {
+            Id = id;
+            DisplayName = displayName;
+        }
+    }
+}
