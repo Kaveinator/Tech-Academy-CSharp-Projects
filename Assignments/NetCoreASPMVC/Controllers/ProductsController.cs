1509f5d0667e613035256ef9a6d81554d0534eca
Kaveman <55042475+Kaveinator@users.noreply.github.com>
2024-04-15 14:46:12 -0700

    Scores and NetCoreASPMVC Assignments

---
+++ Assignments/NetCoreASPMVC/Controllers/ProductsController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreASPMVC.Controllers {
+    public class ProductsController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
---
+++ Assignments/Scores/Program.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Scores {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("First Name: ");
+            string uName = Console.ReadLine(),
+                date = DateTime.Today.ToShortDateString(),
+                msg = $"\nWelcome back {uName}. Today is {date}";
+            Console.WriteLine(msg);
+
+            Console.WriteLine("\nStudent Scores:");
+            string[] lines = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "studentScores.txt"));
+            decimal tScore = 0m;
+            int successfulIterations = 0;
+            foreach (string line in lines) {
+                if (decimal.TryParse(line, out decimal score)) {
+                    Console.WriteLine($" - {score}");
+                    tScore += score;
+                    successfulIterations++;
+                }
+            }
+            decimal avgScore = tScore / (decimal)successfulIterations;
+            Console.WriteLine($"\nTotal of {successfulIterations} student scores." +
+                $"\nAverage Score: {avgScore}" +
+                $"\nFailed Iterations: {successfulIterations - lines.Count()}"
+            );
+
+            Console.WriteLine("\n\nPress any key to exit");
+            _ = Console.ReadKey(true);
+        }
+    }
+}
